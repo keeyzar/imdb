e92385271d7e0b5dd3d7d6e88505237842739c4a
Kevin Kekule <k.kekule@web.de>
2021-09-27 11:36:21 +0200

    intial commit

---
+++ src/main/java/de/keeyzar/checkimdb/imdbclone/ImdbCloneApplication.java
@@ -0,0 +1,13 @@
+package de.keeyzar.checkimdb.imdbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbCloneApplication.class, args);
+	}
+
+}
